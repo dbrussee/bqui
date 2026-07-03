a4417b961f6b1b32359a60d8c9f81b1a5e55909c
Dan Brussee <dbrussee@gmail.com>
2026-07-02 21:54:21 -0400

    add msgs, rights

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { appUserStore } from '../stores/AppUser'
+import { appMessageStore } from '../stores/Messages'
 const userStore = appUserStore()
+const messageStore = appMessageStore()
 const uid = ref('')
 const loginuid = ref('')
 
 async function login() {
   if (loginuid.value != '') {
     await userStore.login(loginuid.value)
-    console.dir(userStore.loggedInUser)
+    console.dir(userStore.user)
+    await messageStore.getMessages(loginuid.value)
+    console.dir(messageStore.messages)
   }
 }
-async function getUser() {
+async function getOtherUser() {
   if (uid.value != '') {
-    await userStore.getUser(uid.value)
-    console.dir(userStore.user)
+    await userStore.getOtherUser(uid.value)
+    console.dir(userStore.otherUser)
   }
 }
 
@@ -26,7 +30,7 @@ const ts = (d: string) => {
     hour: '2-digit',
     minute: '2-digit',
     second: 'numeric',
-    fractionalSecondDigits: 3,
+    // fractionalSecondDigits: 3,
   }).format(new Date(d).getTime())
 }
 </script>
@@ -38,35 +42,39 @@ const ts = (d: string) => {
         <td style="vertical-align: top; width: 30%">
           <input v-model="loginuid" placeholder="Enter User ID" />
           <button @click="login()">Login</button><br />
-          <div v-if="userStore.loggedInUser">
-            Logged In: {{ userStore.loggedInUser.fstnam }} {{ userStore.loggedInUser.lstnam }}<br />
-            Your email is: {{ userStore.loggedInUser.email }}<br />
-            Last Login: {{ ts(userStore.loggedInUser.lst_login) }}<br />
-            Roles: {{ userStore.loggedInUser.config.roles }}
+          <div v-if="userStore.user">
+            Logged In: {{ userStore.user.fstnam }} {{ userStore.user.lstnam }}<br />
+            Your email is: {{ userStore.user.email }}<br />
+            Last Login: {{ ts(userStore.user.lst_login) }}<br />
+            Roles: {{ userStore.user.config.roles }}
           </div>
           <div v-else>Not logged in</div>
         </td>
         <td style="vertical-align: top; padding-left: 10px">
-          <input v-model="uid" placeholder="Enter User ID" />
-          <button @click="getUser()">Load</button><br />
-          <div v-if="userStore.user">
-            Name: {{ userStore.user.fstnam }} {{ userStore.user.lstnam }}<br />
-            Your email is: {{ userStore.user.email }}<br />
-            Last Login: {{ ts(userStore.user.lst_login) }}<br />
-            Roles: {{ userStore.user.config.roles }}
+          <input v-model="uid" placeholder="Enter Other User ID" />
+          <button @click="getOtherUser()">Load</button><br />
+          <div v-if="userStore.otherUser">
+            Name: {{ userStore.otherUser.fstnam }} {{ userStore.otherUser.lstnam }}<br />
+            Your email is: {{ userStore.otherUser.email }}<br />
+            Last Login: {{ ts(userStore.otherUser.lst_login) }}<br />
+            Roles: {{ userStore.otherUser.config.roles }}
           </div>
         </td>
       </tr>
     </tbody>
   </table>
-  <div v-if="userStore.user">
-    <!-- Logged in as: {{ userStore.loggedInUser.fstnam }} {{ userStore.loggedInUser.lstnam }}<br />
-    Took: {{ userStore.meta.times.ms }}ms -->
+  <h3>Messages</h3>
+  <div v-for="msg in messageStore.messages" :key="msg.id">
+    Sent: {{ ts(msg.sentat) }}<br />
+    <b>{{ msg.subject }}</b
+    ><br />
+    <i>{{ msg.msgtxt }}</i>
+    <hr />
   </div>
-  <h2>Messages</h2>
-  <!-- <div v-for="msg in fetcher.json.messages" :key="msg.id">
-    {{ msg.text }}
-  </div> -->
+  <h3>Rights</h3>
+  <div v-for="(value, key) in userStore.user.rights" :key="key">
+    {{ key }}: {{ value.value }} ({{ value.source }})
+  </div>
 </template>
 
 <style scoped></style>
